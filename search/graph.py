a03dc13e600e092fa784bbbad7d8931819816f53
gkim9 <194012171+gkim9@users.noreply.github.com>
2025-01-22 13:21:09 -0800

    updating graph.py and test.py; adding adjlist files to test edge cases

--- search/graph.py
+++ search/graph.py
@@ -11,6 +11,46 @@ class Graph:
         Initialization of graph object 
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+    
+    def print_graph(self):
+        # needed to figure out what nx.read_adjlist did so added a print function
+        return self.graph
+
+    def neighbors(self, node):
+        # with input of the graph and node, return a list of neighbors (directed for that node)
+        # node is cited by the items in the list returned
+        return [neighbor for neighbor in self.graph[node]]
+    
+    def solve_bfs(self, start):
+        '''
+        'Solving' the graph by getting the "parent" of each node
+        (not getting the shortest path from start -> end)
+        '''
+        prev_node = {} # will contain key, val pair of {node: parent node} for all nodes
+        node_list = [] # all nodes
+        for node in self.graph:
+            prev_node[node] = None # initializing dictionary, the start node should have None
+            node_list.append(node)
+        print(len(node_list))
+        if start not in node_list:
+            raise Exception("Either start or end nodes are not found in the graph")
+
+        # based on the pseudocode given in class, added the tracking of the previous/parental node
+        Q = []
+        visited = []
+        Q.append(start)
+        visited.append(start)
+
+        while Q != []:
+            node = Q.pop(0)
+            N_list = self.neighbors(node)
+            for neighbor in N_list:
+                if neighbor not in visited:
+                    visited.append(neighbor)
+                    # print(visited)
+                    Q.append(neighbor)
+                    prev_node[neighbor] = node
+        return visited, prev_node
 
     def bfs(self, start, end=None):
         """
@@ -20,9 +60,22 @@ class Graph:
         * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         * If there is an end node input and a path does not exist, return None
 
+        Things to raise exception for: if start/end nodes are not in graph
+
+        Will call the solve_bfs function that solves the graph with a given start node
+        "BFS" function will determine what outputs to give depending on start/end nodes & compile shortest path if it exists
         """
-        return
-
-
-
-
+        visited, prev_node = self.solve_bfs(start)
+        shortest_path = []
+        input_end = end
+        # if no end, just return the "graph" which is the list of the previous node in reference to a specific start
+        if end == None:
+            return visited
+        while end != start:
+            shortest_path.append(end)
+            end = prev_node[end] # update new end to go up the chain of "parental" nodes until you hit the start
+            if end == None: # No path from the start to end node (since it is guaranteed that if the path exists, it'll be less than the path through all nodes)
+                return None
+        shortest_path.append(end)
+       
+        return shortest_path[::-1] # since appended from end -> start, reversing order
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,6 +1,10 @@
 # write tests for bfs
 import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # copied from last week's to allow local testing
 from search import graph
+import networkx as nx
 
 def test_bfs_traversal():
     """
@@ -10,7 +14,16 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+    #should raise an exception with empty graph input
+    empty_network = graph.Graph('data/empty_network.adjlist')
+    with pytest.raises(Exception):
+        empty_network.bfs('Start')
+
+    # testing with tiny_network
+    tiny_network = graph.Graph('data/tiny_network.adjlist')
+
+    # total number of visited nodes should equal total number of nodes of tiny_network (30)
+    assert len(tiny_network.bfs("Luke Gilbert")) == 30, "Wrong number of nodes and/or edges"
 
 def test_bfs():
     """
@@ -23,4 +36,20 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    pass
+    # testing graph with 1->7 path but no 7->1 path
+    no_connected_network = graph.Graph('data/no_connected_node_test.adjlist')
+    assert no_connected_network.bfs("1") == ['1', '2', '3', '4', '5', '6', '7'], "Did not traverse graph in correct order"
+    assert no_connected_network.bfs("1", "7") == ['1', '2', '3', '4', '5', '6', '7'], "Did not traverse graph in correct order"
+    assert no_connected_network.bfs("7", "1") is None #no path should exist from 7 -> 1
+    
+    network = graph.Graph('data/citation_network.adjlist')
+    assert network.bfs("Luke Gilbert", "34858697") is None, "BFS did not work correctly--> expected no path"
+
+    # Using the networkx package to get "ground truth" of shortest path to compare our code
+
+    actual_shortest = nx.shortest_path(network.graph, "Luke Gilbert", "Lani Wu")
+    our_shortest = network.bfs("Luke Gilbert", "Lani Wu")
+    assert actual_shortest == our_shortest, "Incorrect shortest path"
+
+    different_source = network.bfs("Tony Capra", "Luke Gilbert")
+    assert actual_shortest != different_source, "Path not calculated correctly"
